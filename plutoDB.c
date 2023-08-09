3f9810d453547dcc01f1dc81376b5b08f4f093ab
ColeShirakata <coleshirakata@gmail.com>
2023-08-08 21:48:51 -0700

    Add comments

--- plutoDB.c
+++ plutoDB.c
@@ -67,6 +67,7 @@ typedef struct {
     Row row_to_insert;
 } Statement;
 
+// Allocates space for new input buffer
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
@@ -76,6 +77,7 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+// Frees the memory allocated to the input buffer
 void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
@@ -85,6 +87,8 @@ void print_prompt() {
     printf("pluto > ");
 }
 
+// Finds next open spot for new row
+// If page is full, allocate a new one
 void* row_slot(Table* table, uint32_t row_num) {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
     void* page = table->pages[page_num];
@@ -97,6 +101,9 @@ void* row_slot(Table* table, uint32_t row_num) {
     return page + byte_offset;
 }
 
+// Reads user input
+// Throws EXIT_FAILURE if invalid input
+// Loads user input into the input buffer
 void read_input(InputBuffer* input_buffer) {
     ssize_t bytes = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
@@ -109,6 +116,7 @@ void read_input(InputBuffer* input_buffer) {
     input_buffer->buffer[bytes - 1] = 0;
 }
 
+// Handles the meta commands. Currently only exit.
 MetaCommand do_meta_command(InputBuffer* input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         exit(EXIT_SUCCESS);
@@ -117,18 +125,22 @@ MetaCommand do_meta_command(InputBuffer* input_buffer) {
     }
 }
 
+// Writes data to memory
 void serialize_row(Row* source, void* destination) {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
     memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
     memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
+// Retrieves data from memory
 void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
     memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+// If user inserts, write the id, username, and password to statement. Else throw an error if invalid input.
+// If user selects, make statement STATEMENT_SELECT
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
@@ -151,6 +163,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+// Load row to insert, serialize the row, and add to number of rows
 ExecuteResult execute_insert(Statement* statement, Table* table) {
     if (table->num_rows >= TABLE_MAX_ROWS) {
         return EXECUTE_TABLE_FULL;
@@ -168,6 +181,8 @@ void print_row(Row* row) {
     printf("%d %s %s\n", row->id, row->username, row->email);
 }
 
+// Iterate through each row and deserialize the data
+// Print row
 ExecuteResult execute_select(Statement* statement, Table* table) {
     Row row;
     for (uint32_t i = 0; i < table->num_rows; i++) {
@@ -177,6 +192,7 @@ ExecuteResult execute_select(Statement* statement, Table* table) {
     return EXECUTE_SUCCESS;
 }
 
+// Checks for statement type then loads appropriate operation
 ExecuteResult execute_statement(Statement* statement, Table* table) {
     switch(statement->type) {
         case (STATEMENT_INSERT):
@@ -186,6 +202,8 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
     }
 }
 
+// Allocates memory for new table
+// Sets each new page to null
 Table* new_table() {
     Table* table = (Table*)malloc(sizeof(Table));
     table->num_rows = 0;
@@ -195,6 +213,7 @@ Table* new_table() {
     return table;
 }
 
+// Free each page then free table
 void free_table(Table* table) {
     for (int i = 0; table->pages[i]; i++) {
         free(table->pages[i]);
@@ -210,6 +229,7 @@ int main(int argc, char* argv[]) {
         print_prompt();
         read_input(input_buffer);
 
+        // Checks for meta commands
         if (input_buffer->buffer[0] == '.') {
             switch (do_meta_command(input_buffer)) {
                 case (META_COMMAND_SUCCESS):
@@ -220,6 +240,7 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        // Reads SQL statements
         Statement statement;
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
@@ -233,6 +254,7 @@ int main(int argc, char* argv[]) {
 
         }
 
+        // Handles SQL statements
         switch (execute_statement(&statement, table)) {
             case (EXECUTE_SUCCESS):
                 printf("Executed.\n");
